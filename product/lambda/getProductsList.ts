ae47385de995de7fcfb4e0ea35c83f518151c623
salivahog <tiarted@gmail.com>
2024-06-25 11:02:08 +0200

    feat:add getProductlist lambda

--- product/lambda/getProductsList.ts
+++ product/lambda/getProductsList.ts
@@ -1,10 +1,55 @@
-import { IProduct, productResponse } from "./utility";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand, BatchGetCommand } from "@aws-sdk/lib-dynamodb";
 
-export const handler = async () => {
-  const products: IProduct[] = JSON.parse(process.env.PRODUCTS_DATA ?? "[]");
-  if (!products.length) {
-    return productResponse(404, { message: "No products found" });
+import { productResponse } from "./utility";
+
+const client = new DynamoDBClient({ region: 'eu-central-1' });
+const dynamoDB = DynamoDBDocumentClient.from(client);
+
+const PRODUCTS_TABLE_NAME = process.env.PRODUCTS_TABLE_NAME;
+const STOCKS_TABLE_NAME = process.env.STOCKS_TABLE_NAME;
+
+if (!PRODUCTS_TABLE_NAME || !STOCKS_TABLE_NAME) {
+  throw new Error("Environment variables for table names are not set");
+}
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log(event);
+
+  try {
+    const productsData = await dynamoDB.send(new ScanCommand({ TableName: PRODUCTS_TABLE_NAME }));
+    const products= productsData.Items ;
+
+    if (!products || !products.length) {
+      return productResponse(404, { message: "No products found" });
+    }
+
+    const keys = products.map(product => ({ product_id: product.id }));
+    const stocksData = await dynamoDB.send(new BatchGetCommand({
+      RequestItems: {
+        [STOCKS_TABLE_NAME]: {
+          Keys: keys,
+        },
+      },
+    }));
+
+
+    const enrichedProducts = products.map(product => {
+      const stock = stocksData.Responses?.[STOCKS_TABLE_NAME]?.find(s => s.product_id === product.id);
+      return { ...product, stock: stock ? stock.count : 0 };
+    });
+
+    return productResponse(200, enrichedProducts);
+  } catch (error) {
+    console.error(error);
+    return productResponse(500, { message: "Internal server error" });
+  
   }
-
-  return productResponse(200, products);
 };

2372745d54d6832987a149f157c126164cba5da8
salivahog <tiarted@gmail.com>
2024-07-03 20:50:17 +0200

    feat:add parse file lambda

--- import-service/lambdas/importProductsFile.ts
+++ import-service/lambdas/importProductsFile.ts
@@ -1,24 +1,64 @@
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+import {
+  APIGatewayProxyEvent,
+} from 'aws-lambda';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-exports.handler = async (event) => {
-  const fileName = event.queryStringParameters.name;
+const s3Client = new S3Client({ region: 'eu-west-1' });
+
+const BUCKET = process.env.BUCKET_NAME;
+
+
+
+const commonHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Content-Type": "application/json",
+};
+
+exports.handler = async (event: APIGatewayProxyEvent) => {
+  const fileName = event.queryStringParameters?.name;
+  if (!fileName) {
+    return {
+      statusCode: 400,
+      headers: commonHeaders,
+      body: JSON.stringify({ message: "File name is required" }),
+    };
+  }
+
+  // Additional fileName validation logic can be added here
+
+  if (!BUCKET) {
+    return {
+      statusCode: 500,
+      headers: commonHeaders,
+      body: JSON.stringify({ message: "Server configuration error" }),
+    };
+  }
+
   const s3Params = {
-    Bucket: process.env.BUCKET_NAME,
+    Bucket: BUCKET,
     Key: `uploaded/${fileName}`,
-    Expires: 60, // URL expiration time
+    Expiration: 60, // URL expiration time
   };
 
   try {
-    const signedUrl = await s3.getSignedUrlPromise('putObject', s3Params);
+    const putObjectCommand = new PutObjectCommand(s3Params);
+    const signedUrl = await getSignedUrl(s3Client, putObjectCommand);
     return {
       statusCode: 200,
+      headers: commonHeaders,
       body: JSON.stringify({ url: signedUrl }),
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Could not create a signed URL" }),
+      headers: commonHeaders,
+      body: JSON.stringify({ message: "Error generating signed URL", error: (error as Error).message }),
     };
   }
 };
+
+
+
--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,25 +12,51 @@ export class ImportServiceStack extends cdk.Stack {
 
  
     // Define the S3 bucket
-    const bucket = new s3.Bucket(this, 'ProductsBucket');
+    const bucket = s3.Bucket.fromBucketName(
+      this,
+      'csvbucket',
+      'csvbucket-rsschool'
+    );
 
     // Define the Lambda function
     const importProductsFileLambda = new lambda.Function(this, 'ImportProductsFile', {
       runtime: lambda.Runtime.NODEJS_20_X,
       handler: 'importProductsFile.handler',
-      code: lambda.Code.fromAsset('lambda'),
+      code: lambda.Code.fromAsset('lambdas'),
       environment: {
         BUCKET_NAME: bucket.bucketName,
       },
     });
 
+    importProductsFileLambda.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['s3:PutObject'],
+      resources: [`${bucket.bucketArn}/uploaded/*`]
+    }));
+    const importFileParserLambda = new lambda.Function(this, 'ImportFileParser', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'importFileParser.handler',
+      code: lambda.Code.fromAsset('lambdas'),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
+    });
     // Grant the Lambda function permissions to generate signed URLs
     bucket.grantPut(importProductsFileLambda);
+    bucket.grantReadWrite(importProductsFileLambda);
+
+
 
     // Define the API Gateway
     const api = new apigateway.RestApi(this, 'ImportServiceApi', {
       restApiName: 'Import Service',
     });
+
+
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.LambdaDestination(importFileParserLambda),
+      { prefix: 'uploaded/' }
+    );
 
     // Add the /import resource
     const importResource = api.root.addResource('import');
